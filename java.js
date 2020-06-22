7f735d1ea19e494dd50009f0b72e69c936815f4d
sara362x <sara362x@edu.easj.dk>
2020-06-22 15:22:47 +0200

    Initial commit

---
+++ java.js
@@ -0,0 +1,25 @@
+/* køre gennem alle dropdown knapper så man kan toogle mellem at de er åbne og lukkede. gør så man kan have flere åbne på en gang uden problemer. */
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var dropdownContent = this.nextElementSibling;
+  if (dropdownContent.style.display === "block") {
+  dropdownContent.style.display = "none";
+  } else {
+  dropdownContent.style.display = "block";
+  }
+  });
+}
+
+/*Dette er til bruger menuen. den første linje viser rød herinde, men det ser ud som om det virker?*/
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
